221fa03c3c7135bf03b7b45ad60fd0d47f0059e1
rocionadal <rocioonadal@gmail.com>
2021-03-26 15:43:09 +0100

    js modules

---
+++ assets/scripts/App/Component.js
@@ -0,0 +1,25 @@
+export class Component {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId) {
+      this.hostElementId = document.getElementById(hostElementId);
+    } else {
+      this.hostElementId = document.body;
+    }
+    this.insertBefore = insertBefore;
+  }
+
+  detach() {
+    if (this.element) {
+      this.element.remove();
+    }
+    // this.element.parentElement.removeChild(this.element); // for older browser support
+  }
+
+  attach() {
+    this.hostElementId.insertAdjacentElement(
+      this.insertBefore ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+}
+
